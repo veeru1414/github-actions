b6eb08fb552c683b0e5991957d2aa38414d5b186
veeru1414 <102665540+veeru1414@users.noreply.github.com>
2023-03-20 16:37:44 +0530

    Create cleanup.sh
    Created cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# delete queue manager
+oc delete -n cp4i qmgr qm1
+rm qm1-qmgr.yaml
+
+# delete config map
+oc delete -n cp4i cm example-01-qm1-configmap
+rm qm1-configmap.yaml
+
+# delete route
+oc delete -n cp4i route example-01-qm1-route
+rm qm1chl-route.yaml
+
+# delete secret
+oc delete -n cp4i secret example-01-qm1-secret
+
+# delete files
+rm qm1.crt qm1.key app1key.* ccdt.json
