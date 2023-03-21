ca45d330f821d5c7b925d0d4f6266beee25a3bfb
veeru1414 <102665540+veeru1414@users.noreply.github.com>
2023-03-21 13:48:42 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Deleting queue manager...."
-oc delete -n cp4i qmgr qm1
+oc delete -n $1 qmgr qm1
 
 echo "Deleting config map...."
-oc delete -n cp4i cm example-01-qm1-configmap
+oc delete -n $1 cm example-01-qm1-configmap
 
 
 echo "Deleting route...."
-oc delete -n cp4i route example-01-qm1-route
+oc delete -n $1 route example-01-qm1-route
 
 echo "Deleting secret...."
-oc delete -n cp4i secret example-01-qm1-secret
+oc delete -n $1 secret example-01-qm1-secret
